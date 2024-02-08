a80620bb95c1d88e8c8acaf31f9941485775b28c
hikannang <noelleonggg@gmail.com>
2024-02-08 13:02:11 +0800

    Changing to 659 instead

--- static/JavaScript/index.js
+++ static/JavaScript/index.js
@@ -1,15 +1,15 @@
 function selectRed(){
-    //658
+    //659
     startCompass()
-    target.latitude = 1.4006938;
-    target.longitude = 103.7388678;
+    target.latitude = 1.4012795;
+    target.longitude = 103.7390489;
 
     if (document.getElementById('redOff')){
         // Turning Red On
         document.getElementById('redOff').id = 'red';
         var content = document.getElementById('red')
         // content.innerHTML = '<a-entity id="red" material="color: red" geometry="primitive: box" gps-projected-entity-place="latitude: 1.3068866777147514; longitude: 103.84940595788129" scale="10 10 10"></a-entity>'
-        content.innerHTML = '<a-entity id="red" gltf-model="./static/3D-file/Assets from Users/BicycleCrossing.glb" scale="2 2 2" gps-projected-entity-place="latitude: 1.4006938; longitude:103.7388678" animation-mixer/>'
+        content.innerHTML = '<a-entity id="red" gltf-model="./static/3D-file/Assets from Users/BicycleCrossing.glb" scale="2 2 2" gps-projected-entity-place="latitude: 1.4012795; longitude:103.7390489" animation-mixer/>'
     }
 
     if(document.getElementById('green')){
