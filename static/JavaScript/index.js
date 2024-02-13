2f8271d35725b8e39d28e63f4ca9fa8c8740630b
hikannang <noelleonggg@gmail.com>
2024-02-13 17:10:27 +0800

    Show Modal

--- static/JavaScript/index.js
+++ static/JavaScript/index.js
@@ -310,13 +310,23 @@ function toggleCircles() {
     }
 }
 
-function showCustomImage() {
-    $('#customModal').modal('show');
+function showModal() {
+    var modal = document.getElementById("customModal");
+    modal.style.display = "block";
 }
 
-function hideCustomImage() {
-    $('#customModal').modal('hide');
+function hideModal() {
+    var modal = document.getElementById("customModal");
+    modal.style.display = "none";
 }
+
+// Close the modal if the user clicks outside of it
+window.onclick = function(event) {
+    var modal = document.getElementById("customModal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
 
 // starts updating the UI.
 function updateUI() {
