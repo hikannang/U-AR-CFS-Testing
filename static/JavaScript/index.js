2600ebfedf13dace126a9a6c51f92f0b641238bf
hikannang <noelleonggg@gmail.com>
2024-02-15 20:24:53 +0800

    try out

--- static/JavaScript/index.js
+++ static/JavaScript/index.js
@@ -54,7 +54,7 @@ function selectGreen(){
         document.getElementById('greenOff').id = 'green';
         var content = document.getElementById('green')
         // content.innerHTML = '<a-entity id="green" material="color: green" geometry="primitive: box" gps-projected-entity-place="latitude: 1.3082540241124714; longitude: 103.84929645038089" scale="10 10 10"></a-entity>'
-        content.innerHTML = '<a-image class="look-at-entity" id="green" src="./static/images/2D assets from users/CyclingPath_4k.png" scale="10 10 10" gps-projected-entity-place="latitude: 1.308538; longitude: 103.849886"></a-image>';
+        content.innerHTML = '<a-image id="green" src="./static/images/2D assets from users/CyclingPath_4k.png" scale="10 10 10" gps-projected-entity-place="latitude: 1.308538; longitude: 103.849886" look-at="[camera]"></a-image>';
     }
 
     if(document.getElementById('red')){
@@ -98,7 +98,7 @@ function selectYellow(){
         // Turning Yellow On
         document.getElementById('yellowOff').id = 'yellow';
         var content = document.getElementById('yellow')
-        content.innerHTML = '<a-image class="look-at-entity" id="yellow" src="./static/images/2D assets from users/CyclingPath_4k.png" scale="10 10 10" gps-projected-entity-place="latitude: 1.401492; longitude: 103.749418"></a-image>';
+        content.innerHTML = '<a-image  id="yellow" src="./static/images/2D assets from users/CyclingPath_4k.png" scale="10 10 10" gps-projected-entity-place="latitude: 1.401492; longitude: 103.749418" look-at="[camera]"></a-image>';
 }
 
     if(document.getElementById('red')){
@@ -142,7 +142,7 @@ function selectBlue(){
         // Turning Yellow On
         document.getElementById('blueOff').id = 'blue';
         var content = document.getElementById('blue')
-        content.innerHTML = '<a-entity class="look-at-entity" id="blue" gltf-model="./static/3D-file/Updated Assets from users/bicycleCrossing_textured.glb" scale="10 10 10" gps-projected-entity-place="latitude: 1.401492; longitude: 103.749418" animation-mixer  rotation-reader></a-entity>';
+        content.innerHTML = '<a-entity id="blue" gltf-model="./static/3D-file/Updated Assets from users/bicycleCrossing_textured.glb" scale="10 10 10" gps-projected-entity-place="latitude: 1.401492; longitude: 103.749418" animation-mixer look-at="[camera]"></a-entity>';
 }
     
 
